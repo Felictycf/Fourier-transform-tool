a9fe4b977f9e0f6b70fe83e3cb55aff8152161b5
Forward <1523832609@qq.com>
2024-09-06 17:46:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -232,3 +232,151 @@ class MyApp(QMainWindow):
                 self.view.setScene(self.scene)
 
         self.canvas.draw()
+
+    def draw_rose_chart(self, prior_prob, true_positive_rate, false_positive_rate, male_ratio,categories):
+        print(categories)
+        posterior_positive, p_positive, p_negative, posterior_negative, posterior_healthy_negative = self.calculate_all_probabilities(
+            prior_prob, true_positive_rate, false_positive_rate)
+        male_posterior_positive, female_posterior_positive = self.calculate_gender_specific_probabilities(prior_prob,
+                                                                                                          true_positive_rate,
+                                                                                                          false_positive_rate,
+                                                                                                          male_ratio)
+
+        sizes1 = [posterior_positive, 1 - posterior_positive]
+        sizes2 = [p_positive, p_negative]
+        sizes3 = [posterior_negative, posterior_healthy_negative]
+        sizes_gender_specific = [male_posterior_positive, female_posterior_positive]
+
+        # Gets the label from the categories parameter
+        labels1 = [categories[0], categories[1]]
+        labels2 = [categories[2], categories[3]]
+        labels3 = [categories[4], categories[5]]
+        labels_gender_specific = ['Male P(D+|T+)', 'Female P(D+|T+)']
+
+        colors1 = ['lightcoral', 'lightskyblue']
+        colors2 = ['gold', 'lightgreen']
+        colors3 = ['lightpink', 'lightblue']
+        colors_gender_specific = ['blue', 'pink']
+
+        explode = (0.1, 0)
+
+        # Clear the previous graphics and reset the canvas
+        if self.scene:
+            self.scene.clear()
+        self.figure, self.ax = plt.subplots(1, 3, figsize=(16, 4))  # 更新为1行4列布局
+
+        self.canvas = FigureCanvas(self.figure)
+
+        self.ax[0].pie(sizes1, explode=explode, labels=labels1, colors=colors1, autopct='%1.1f%%', shadow=True,
+                       startangle=140)
+        self.ax[1].pie(sizes2, explode=explode, labels=labels2, colors=colors2, autopct='%1.1f%%', shadow=True,
+                       startangle=140)
+        self.ax[2].pie(sizes3, explode=explode, labels=labels3, colors=colors3, autopct='%1.1f%%', shadow=True,
+                       startangle=140)
+
+
+
+        self.ax[0].set_title('Posterior Probability Given Positive Test')
+        self.ax[1].set_title('Overall Test Result Probability')
+        self.ax[2].set_title('Posterior Probability Given Negative Test')
+        # self.ax[3].set_title('Gender-Specific Posterior Probability')
+
+        plt.axis('equal')
+
+        if self.scene:
+            self.scene.addWidget(self.canvas)
+            if self.view:
+                self.view.setScene(self.scene)
+
+        self.canvas.draw()
+    def draw_horizontal_bar_chart(self,categories, values,):
+        # Clear scene
+        self.scene.clear()
+
+        self.placeholder1 = self.ele['widget']
+        self.figure, self.ax = plt.subplots()
+        self.canvas = FigureCanvas(self.figure)
+        self.canvas.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        self.canvas.setMinimumSize(550, 400)
+        self.canvas.setMaximumSize(550, 400)
+        self.view = self.ele['graphicsView']
+        self.scene = QGraphicsScene(self)
+        self.view.setScene(self.scene)
+
+
+        layout = self.ele['verticalLayout']
+        layout.addWidget(self.view)
+        self.scene.addWidget(self.view)
+        self.ax.spines['top'].set_visible(False)
+        self.ax.spines['right'].set_visible(False)
+
+
+        self.ax.barh(categories, values, color=self.colors,height=0.4 )
+
+
+        self.ax.set_title(' Bayesian')
+        self.ax.set_xlabel('num')
+        self.ax.set_ylabel('item')
+
+
+        self.canvas.draw()
+    def draw_line_chart(self, categories, values):
+        self.scene.clear()
+
+        self.placeholder1 = self.ele['widget']
+        self.figure, self.ax = plt.subplots()
+        self.canvas = FigureCanvas(self.figure)
+        self.canvas.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        self.canvas.setMinimumSize(550, 400)
+        self.canvas.setMaximumSize(550, 400)
+        self.view = self.ele['graphicsView']
+        self.scene = QGraphicsScene(self)
+        self.view.setScene(self.scene)
+
+        layout = self.ele['verticalLayout']
+        layout.addWidget(self.view)
+        self.scene.addWidget(self.view)
+
+        self.ax.spines['top'].set_visible(False)
+        self.ax.spines['right'].set_visible(False)
+        self.ax.plot(categories, values, marker='o',   markersize=10,  color='skyblue')
+
+
+        self.ax.set_title(' Bayesian')
+        self.ax.set_xlabel('item')
+        self.ax.set_ylabel('num')
+
+
+        self.canvas.draw()
+
+    def calculate_all_probabilities(self, prior_prob, true_positive_rate, false_positive_rate):
+        p_positive = (true_positive_rate * prior_prob) + (false_positive_rate * (1 - prior_prob))
+        posterior_positive = (true_positive_rate * prior_prob) / p_positive
+        p_negative = 1 - p_positive
+        true_negative_rate = 1 - false_positive_rate
+        # posterior_negative = (false_positive_rate * (1 - prior_prob)) / p_negative
+        posterior_negative = ((1 - true_positive_rate) * prior_prob) / p_negative
+
+        posterior_healthy_negative = true_negative_rate * (1 - prior_prob) / p_negative
+        return posterior_positive, p_positive, p_negative, posterior_negative, posterior_healthy_negative
+
+    def calculate_gender_specific_probabilities(self, prior_prob, true_positive_rate, false_positive_rate, male_ratio):
+        female_ratio = 1 - male_ratio
+        posterior_positive, _, _, _, _ = self.calculate_all_probabilities(prior_prob, true_positive_rate,
+                                                                          false_positive_rate)
+        male_posterior_positive = posterior_positive * male_ratio
+        female_posterior_positive = posterior_positive * female_ratio
+        return male_posterior_positive, female_posterior_positive
+
+
+
+    def chooseColorFromPalette(self):
+        for i in range(len(self.colors)):
+            color = QColorDialog.getColor()
+            if color.isValid():
+                if color.name() in self.colors:
+                    continue
+                self.colors[i] = color.name()
+        print(self.colors)
+
+
